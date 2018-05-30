94e1c65521398089882b3d96d77b6b5f02af0811
Adrien Humiliere <adrien.humiliere@capitainetrain.com>
2018-05-30 23:16:26 +0200

    Initial project with some code

---
+++ kmh574/StatusMenuController.swift
@@ -0,0 +1,58 @@
+import Cocoa
+import CoreWLAN
+
+class StatusMenuController: NSObject {
+    @IBOutlet var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+    let wifiAPI = WifiAPI()
+
+    @IBAction func clickedQuit(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+
+    override func awakeFromNib() {
+        statusItem.menu = statusMenu
+        statusItem.highlightMode = true
+        statusItem.configure(with: nil)
+
+        refreshSpeed()
+    }
+
+    @objc func refreshSpeed() {
+        if let wifiNetwork = CWWiFiClient.shared().interface()?.ssid(),
+            wifiNetwork == "_SNCF_WIFI_INOUI" {
+            wifiAPI.fetchSpeed { speed in
+                guard let speed = speed else {
+                    DispatchQueue.main.async {
+                        self.statusItem.configure(with: nil)
+                        self.launchTimer()
+                    }
+                    return
+                }
+
+                let value = NSMeasurement(doubleValue: speed, unit: UnitSpeed.metersPerSecond)
+                let formatter = MeasurementFormatter()
+                formatter.numberFormatter.maximumFractionDigits = 1
+
+                DispatchQueue.main.async {
+                    self.statusItem.configure(with: formatter.string(from: value as Measurement<Unit>))
+                    self.launchTimer()
+                }
+            }
+        } else {
+            self.launchTimer()
+        }
+    }
+
+    func launchTimer() {
+        self.perform(#selector(refreshSpeed), with: nil, afterDelay: 5)
+    }
+}
+
+extension NSStatusItem {
+    func configure(with title: String?) {
+        self.title = title
+        self.isVisible = title == nil ? false : true
+    }
+}
